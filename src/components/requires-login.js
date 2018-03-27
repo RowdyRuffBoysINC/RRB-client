db439e74febbd0ef9da1595d15394fb6ff438ac9
Brent Guistwite <brentguistwite@gmail.com>
2018-03-26 20:00:59 -0400

    format code

--- src/components/requires-login.js
+++ src/components/requires-login.js
@@ -1,27 +1,28 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {Redirect} from 'react-router-dom';
+import { connect, } from 'react-redux';
+import { Redirect, } from 'react-router-dom';
 
-export default () => Component => {
-    function RequiresLogin(props) {
-        const {authenticating, loggedIn, error, ...passThroughProps} = props;
-        if (authenticating) {
-            return <div>Logging in...</div>;
-        } else if (!loggedIn || error) {
-            return <Redirect to="/" />;
-        }
+export default () => (Component) => {
+ function RequiresLogin(props) {
+  const { authenticating, loggedIn, error, ...passThroughProps } = props;
+  if (authenticating) {
+   return <div>Logging in...</div>;
+  }
+  else if (!loggedIn || error) {
+   return <Redirect to="/" />;
+  }
 
-        return <Component {...passThroughProps} />;
-    }
+  return <Component {...passThroughProps} />;
+ }
 
-    const displayName = Component.displayName || Component.name || 'Component';
-    RequiresLogin.displayName = `RequiresLogin(${displayName})`;
+ const displayName = Component.displayName || Component.name || 'Component';
+ RequiresLogin.displayName = `RequiresLogin(${displayName})`;
 
-    const mapStateToProps = (state, props) => ({
-        authenticating: state.auth.loading,
-        loggedIn: state.auth.currentUser !== null,
-        error: state.auth.error
-    });
+ const mapStateToProps = (state, props) => ({
+  authenticating: state.auth.loading,
+  loggedIn: state.auth.currentUser !== null,
+  error: state.auth.error,
+ });
 
-    return connect(mapStateToProps)(RequiresLogin);
+ return connect(mapStateToProps)(RequiresLogin);
 };
--- src/local-storage.js
+++ src/local-storage.js
@@ -1,15 +1,21 @@
 export const loadAuthToken = () => {
-    return localStorage.getItem('authToken');
+ return localStorage.getItem('authToken');
 };
 
-export const saveAuthToken = authToken => {
-    try {
-        localStorage.setItem('authToken', authToken);
-    } catch (e) {}
+export const saveAuthToken = (authToken) => {
+ try {
+  localStorage.setItem('authToken', authToken);
+ }
+ catch (e) {
+  console.err(e);
+ }
 };
 
 export const clearAuthToken = () => {
-    try {
-        localStorage.removeItem('authToken');
-    } catch (e) {}
+ try {
+  localStorage.removeItem('authToken');
+ }
+ catch (e) {
+  console.err(e);
+ }
 };
