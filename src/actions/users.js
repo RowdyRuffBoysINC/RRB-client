c002788cdf0b5862da92459b93449d030eaeef31
Brent Guistwite <brentguistwite@gmail.com>
2018-03-27 09:19:47 -0400

    turn register user action into async await

--- src/actions/users.js
+++ src/actions/users.js
@@ -3,21 +3,23 @@ import { SubmissionError, } from 'redux-form';
 import { API_BASE_URL, } from '../config';
 import { normalizeResponseErrors, } from './utils';
 
-export const registerUser = user => (dispatch) => {
-  return fetch(`${API_BASE_URL}/users`, {
-    method: 'POST',
-    headers: { 'content-type': 'application/json', },
-    body: JSON.stringify(user),
-  })
-    .then(res => normalizeResponseErrors(res))
-    .then(res => res.json())
-    .catch((err) => {
-      const { reason, message, location, } = err;
-      if (reason === 'ValidationError') {
-        // Convert ValidationErrors into SubmissionErrors for Redux Form
-        return Promise.reject(
-          new SubmissionError({ [location]: message, })
-        );
-      }
-    });
+export const registerUser = user => async (dispatch) => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/users`, {
+      method: 'POST',
+      headers: { 'content-type': 'application/json', },
+      body: JSON.stringify(user),
+    })
+    return await response.json();
+  }
+  catch (err) {
+    const { reason, message, location, } = err;
+    if (reason === 'ValidationError') {
+      // Convert ValidationErrors into SubmissionErrors for Redux Form
+      return Promise.reject(
+        new SubmissionError({ [location]: message, })
+      );
+    }
+  };
+
 };
