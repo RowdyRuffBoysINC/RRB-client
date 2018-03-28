a90238ed7e4b26b5b585ff02eefba46821d2ac82
Abraham Cuenca <websiteprojectmanager@gmail.com>
2018-03-28 03:50:22 -0700

    Add loggers, event handlers and wire up buttons

--- src/components/webcam.js
+++ src/components/webcam.js
@@ -11,11 +11,12 @@ export class WebCam extends React.Component {
   }
 
   _init() {
+ 
+//#region Helpers
     /**
     * Helper functions to get this to work
     **/
 
-    
     const trace = (text) => {
       text = text.trim();
       const now = (window.performance.now() / 1000).toFixed(3);
@@ -29,17 +30,31 @@ export class WebCam extends React.Component {
     const getOtherPeer = (peerConnection) => {
       return (peerConnection === localPeerConnection) ? remotePeerConnection : localPeerConnection;
     };
-
+ 
     const randomToken = () =>{
       return Math.floor((1+Math.random())*1e16).toString(16).substring(1);
     };
+
+//#endregion
+
+    let startTime = null;
+    const localVideo = document.getElementById('webCam-localVideo');
+    const remoteVideo = document.getElementById('webCam-remoteVideo');
+
+    let localStream;
+    let remoteStream;
+
+    let localPeerConnection;
+    let remotePeerConnection;
+
+  localVideo.addEventListener('loadedmetadata', logVideoLoaded);
+  remoteVideo.addEventListener('loadedmetadata', logVideoLoaded);
+  remoteVideo.addEventListener('onresize', logResizedVideo);
 
     const mediaStreamConstraints = {
       video: true,
     };
 
-    const localVideo = document.querySelector('video#webCam-localVideo');
-    
     const gotLocalMediaStream = (mediaStream) => {
       localVideo.srcObject = mediaStream;
     }
@@ -50,6 +65,192 @@ export class WebCam extends React.Component {
 
     navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
       .then(gotLocalMediaStream).catch(handleLocalMediaStreamError)
+  
+  const offerOptions = {
+    offerToReceiveVideo: 1,
+  };
+
+
+
+  const gotLocalStreamMediaStream = (mediaStream) => {
+    localVideo.srcObject = mediaStream;
+    localStream = mediaStream;
+    trace('Received local stream.');
+    callButton.disabled = false;
+  }
+
+  const gotRemoteMediaStream = (event) => {
+    const mediaStream = event.stream;
+    remoteVideo.srcObject = mediaStream;
+    remoteStream = mediaStream;
+    trace('Remote peer connection received remote stream.');
+  }
+
+    function logVideoLoaded(event){
+    const video = event.target;
+    trace(`${video.id} videoWidth: ${video.videoWidth}px, videoHeight: ${video.videoHeight}px`);
+  }
+
+  function logResizedVideo(event){
+    logVideoLoaded(event);
+    if(startTime){
+      const elapsedTime = window.performance.now() - startTime;
+      startTime = null;
+      trace(`Setup time: ${elapsedTime.toFixed(3)}ms.`);
+    }
+  }
+
+
+
+  const handleConnection = (event) => {
+    const peerConnection = event.target;
+    const iceCandidate = event.candidate;
+
+    if(iceCandidate){
+      const newIceCandidate = new RTCIceCandidate(iceCandidate);
+      const otherPeer = getOtherPeer(peerConnection);
+
+      otherPeer.addIceCandidate(newIceCandidate)
+        .then(() =>{
+          handleConnectionSuccess(peerConnection);
+        }).catch((error) =>{
+          handleConnectionFailure(peerConnection.error)
+        });
+
+        trace(`${getPeerName(peerConnection)} ICE candidate:\n ${event.candidate.candidate}`);
+    }
+  }
+
+  function handleConnectionSuccess(peerConnection){
+    trace(`${getPeerName(peerConnection)} addIceCandidate success.`);
+  }
+
+  function handleConnectionFailure(peerConnection, error){
+    trace(`${getPeerName(peerConnection)} failed to add ICE Candidate:\n ${error.toString()}.`);
+  }
+
+  function handleConnectionChange(event){
+    const peerConnection = event.target;
+    console.log('ICE state change event: ', event);
+    trace('${getPeerName(peerConnection)} ICE state: ${peerConnection.iceConnectionState}');
+  }
+
+  function setSessionDescriptionError(error){
+    trace('Failed to create session description ${error.toString()}.');
+  }
+
+  function setDescriptionSuccess(peerConnection, functionName){
+    const peerName = getPeerName(peerConnection);
+    trace(`${peerName} ${functionName} complete.`);
+  }
+
+  function setLocalDescriptionSuccess(peerConnection){
+    setDescriptionSuccess(peerConnection, 'setLocalDescription');
+  }
+
+  function setRemoteDescriptionSuccess(peerConnection){
+    setDescriptionSuccess(peerConnection, 'setRemoteDescription');
+  }
+
+  const createdOffer = (description) => {
+    trace(`Offer from localPeerConnection:\n${description.sdp}`);
+    trace(`localPeerConnection setLocalDescription start.`);
+    localPeerConnection.setLocalDescription(description)
+      .then(() => {
+        setLocalDescriptionSuccess(localPeerConnection);
+      }).catch(setSessionDescriptionError);
+
+      trace('remotePeerConnection setRemoteDescription start.');
+      remotePeerConnection.setRemoteDescription(description)
+        .then(() => {
+          setRemoteDescriptionSuccess(remotePeerConnection);
+        }).catch(setSessionDescriptionError);
+
+        trace('remotePeerConnection createAnswer start.');
+        remotePeerConnection.createAnswer()
+          .then(createdAnswer)
+          .catch(setSessionDescriptionError);
+  }
+
+  function createdAnswer(description){
+    trace(`Answer from remotePeerConnection:\n${description.sdp}`);
+    trace(`remotePeerConnection setLocalDescription start.`);
+    remotePeerConnection.setLocalDescription(description)
+      .then(() => {
+        setLocalDescriptionSuccess(remotePeerConnection);
+      }).catch(setSessionDescriptionError);
+
+      trace('localPeerConnection setRemoteDescription start.');
+      localPeerConnection.setRemoteDescription(description)
+        .then(() =>{
+          setRemoteDescriptionSuccess(localPeerConnection);
+        }).catch(setSessionDescriptionError);
+  }
+
+  const startButton = document.getElementById('webCam-startButton');
+  const callButton = document.getElementById('webCam-callButton');
+  const hangupButton = document.getElementById('webCam-hangupButton');
+
+  callButton.disabled= true;
+  hangupButton.disabled = true;
+
+  const startAction = () =>{
+    startButton.disabled = true;
+    navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
+      .then(gotLocalMediaStream).catch(handleLocalMediaStreamError);
+      trace(`Requesting local stream.`);
+  }
+
+  const callAction = ()=> {
+    callButton.disabled = true;
+    hangupButton.disabled = false;
+    trace('Starting call.');
+    startTime = window.performance.now();
+
+    const videoTracks = localStream.getVideoTracks();
+    const audioTracks = localStream.getAudioTracks();
+
+    if (videoTracks.length > 0) trace(`Using video device: ${videoTracks[0].label}.`);
+    
+    if (audioTracks.length > 0) trace(`Using audio device: ${audiTracks[0].label}`) ;
+
+    const servers = null;
+
+    localPeerConnection = new RTCPeerConnection(servers);
+    trace('Created local peer connection object localPeerConnection');
+
+    localPeerConnection.addEventListener('icecandidate', handleConnection);
+    localPeerConnection.addEventListener('iceconnectionstatechange', handleConnectionChange);
+
+    remotePeerConnection = new ETCPeerConnection(servers);
+    trace('Created remote peer connection object RemotePeerConnection.');
+
+    remotePeerConnection.addEventListener('icecandidate', handleConnection);
+    remotePeerConnection.addEventListener('iceconnectionstatechange', handleConnectionChange);
+    remotePeerConnection.addEventListener('addstream', gotRemoteMediaStream);
+
+    localPeerConnection.addStream(localStream);
+    trace('Added local stream to localPeerConnection.');
+
+    trace('localPeerConnection createOffer start.');
+    localPeerConnection.createOffer(offerOptions)
+      .then(createdOffer).catch(setSessionDescriptionError);
+  }
+
+  const hangupAction = () => {
+    localPeerConnection.close();
+    remotePeerConnection.close();
+    localPeerConnection = null;
+    remotePeerConnection = null;
+    hangupButton.disabled = true;
+    callButton.disabled = false;
+    trace('Ending call.')
+  }
+
+  startButton.addEventListener('click', startAction);
+  callButton.addEventListener('click', callAction);
+  hangupButton.addEventListener('click', hangupAction);
+
   }
 
   render() {
