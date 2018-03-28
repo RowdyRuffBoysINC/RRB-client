de6fe755e0048c0a068b38e7812f99aac2830179
Vernon Mensah <vernonmensah@gmail.com>
2018-03-28 10:58:43 -0400

    added doc editor

--- src/components/applicationPage/Editors/WordEditor.js
+++ src/components/applicationPage/Editors/WordEditor.js
@@ -1,26 +1,87 @@
-import React from 'react';
-import { connect, } from 'react-redux';
+import React, { Component } from "react";
+// import ReactDOM from 'react-dom';
 
-export class WordEditor extends React.Component {
-  componentDidMount() {}
+// Require Editor JS files.
+import "froala-editor/js/froala_editor.pkgd.min.js";
+
+// Require Editor CSS files.
+import "froala-editor/css/froala_style.min.css";
+import "froala-editor/css/froala_editor.pkgd.min.css";
+import "froala-editor/css/themes/dark.min.css";
+
+// Require Font Awesome.
+import "font-awesome/css/font-awesome.css";
+
+// Css for wordEditor
+import "./WordEditor.css";
+
+import FroalaEditor from "react-froala-wysiwyg";
+import FroalaEditorView from "react-froala-wysiwyg/FroalaEditorView";
+
+import $ from "jquery";
+window.$ = $;
+
+class WordEditor extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      content: 'Insert text here =]'
+    };
+
+    this.config = {
+      iframe: false,
+      fullPage: true,
+      htmlAllowedTags: [".*"],
+      htmlAllowedAttrs: [".*"],
+      htmlAllowedStyleProps: [".*"],
+      htmlRemoveTags: ["script", "head"],
+      pasteAllowedStyleProps: [".*"],
+      pasteDeniedAttrs: [],
+      lineBreakerOffset: 0,
+      lineBreakerTags: [""],
+      htmlAllowComments: true,
+      events: {
+        "froalaEditor.initialized": function(e, editor, html) {
+          console.log("START!");
+          console.log(html);
+        },
+        "froalaEditor.commands.after": function(e, editor, html) {
+          console.log("COMMANDS AFTER!");
+          console.log(html);
+        },
+        "froalaEditor.keydown": function(e, editor, html) {
+          console.log("key down");
+        },
+        "froalaEditor.keyup": function(e, editor, html) {
+          console.log("key up");
+        }
+      }
+    };
+  }
+
+  componentWillMount() {}
+
+  handleModelChange = model => {
+    console.log("X.X modelChange", model);
+    this.setState({ content: model });
+  };
 
   render() {
-
     return (
-      <section>
-        <h3>Google Docs </h3>
-      </section>
+      <div className="main">
+        <div className="editorBox">
+          <FroalaEditor
+            tag="textarea"
+            config={this.config}
+            model={this.state.content}
+            onModelChange={model => this.handleModelChange(model)}
+          />
+        </div>
+      </div>
     );
   }
 }
 
-const mapStateToProps = state => {
-  const { currentUser } = state.auth;
-  return {
-    username: state.auth.currentUser.username,
-    name: `${currentUser.firstName} ${currentUser.lastName}`,
-    protectedData: state.protectedData.data
-  };
-};
-
-export default connect(mapStateToProps)(WordEditor);
+export default WordEditor;
