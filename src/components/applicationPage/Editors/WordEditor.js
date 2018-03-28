9d0f0deca9b60d06c721ff72f09480a80fc8a4bd
Vernon Mensah <vernonmensah@gmail.com>
2018-03-28 13:42:36 -0400

    added react draft editor

--- src/components/applicationPage/Editors/WordEditor.js
+++ src/components/applicationPage/Editors/WordEditor.js
@@ -1,12 +1,30 @@
-import React, { Component } from "react";
+import React, { Component, } from 'react';
+import { EditorState, } from "draft-js";
+import { Editor, } from 'react-draft-wysiwyg';
+
+import './react-draft-wysiwyg.css';
 
 class WordEditor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editorState: EditorState.createEmpty(),
+    };
+  }
 
+  onEditorStateChange = (editorState) => {
+    this.setState({ editorState, });
+  }
 
   render() {
+    const { editorState, } = this.state;
     return (
-      <div className="main">
-      </div>
+      <Editor
+        editorState={editorState}
+        wrapperClassName="demo-wrapper"
+        editorClassName="demo-editor"
+        onEditorStateChange={this.onEditorStateChange}
+      />
     );
   }
 }
