9479cad17d4b03c9581219577d101579599fe6ae
Vernon Mensah <vernonmensah@gmail.com>
2018-03-29 12:06:14 -0400

    #25 edited package.json + imports

--- src/components/applicationPage/Editors/WhiteBoardEditor.js
+++ src/components/applicationPage/Editors/WhiteBoardEditor.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { UnControlled as CodeMirror, } from 'react-codemirror2';
 import { connect, } from 'react-redux';
+import { socket, } from '../Room';
+import { SketchField, Tools } from "react-sketch";
 
 export class WhiteBoardEditor extends React.Component {
   componentDidMount() { }
@@ -13,12 +16,13 @@ export class WhiteBoardEditor extends React.Component {
   }
 }
 
-const mapStateToProps = state => {
-  const { currentUser } = state.auth;
+const mapStateToProps = (state) => {
+  const { currentUser, } = state.auth;
   return {
     username: state.auth.currentUser.username,
     name: `${currentUser.firstName} ${currentUser.lastName}`,
-    protectedData: state.protectedData.data
+    protectedData: state.protectedData.data,
+    roomName: state.applicationReducer.roomName,
   };
 };
 
