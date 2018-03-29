dc27d4c65443e28fb8b497eec041d722911f8519
Vernon Mensah <vernonmensah@gmail.com>
2018-03-29 08:06:21 -0400

    #23 linter screaming

--- src/components/applicationPage/Room.js
+++ src/components/applicationPage/Room.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { connect, } from 'react-redux';
 import io from 'socket.io-client';
-import { withRouter } from 'react-router'
+import { withRouter, } from 'react-router';
 import EditorView from './EditorView';
-import { setCreateInput } from '../../actions/application';
-import { API_BASE_URL } from '../../config';
+import { setCreateInput, } from '../../actions/application';
+import { API_BASE_URL, } from '../../config';
 export const socket = io(API_BASE_URL);
 
 export class Room extends React.Component {
   componentDidMount() {
     this.props.dispatch(setCreateInput(this.props.match.params.roomName));
-    socket.emit('join room', {room: this.props.match.params.roomName, user: this.props.username});
+    socket.emit('join room', {room: this.props.match.params.roomName, user: this.props.username,});
   }
 
   componentWillUnmount() {
-    socket.emit('leave room', {room: this.props.match.params.roomName, user: this.props.username});
+    socket.emit('leave room', {room: this.props.match.params.roomName, user: this.props.username,});
   }
 
   render() {
