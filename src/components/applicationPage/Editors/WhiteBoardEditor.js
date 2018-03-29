6ad2534a8a26756f8b3bbadc1cb7f1a4c815798b
Vernon Mensah <vernonmensah@gmail.com>
2018-03-29 16:02:01 -0400

    start of socket of logic

--- src/components/applicationPage/Editors/WhiteBoardEditor.js
+++ src/components/applicationPage/Editors/WhiteBoardEditor.js
@@ -7,15 +7,81 @@ import { SketchField, Tools, } from 'react-sketch';
 import './WhiteBoardEditor.css';
 
 export class WhiteBoardEditor extends React.Component {
+  constructor() {
+    super();
+    this.state = { sketchFieldValue: {},};
+
+    socket.on("whiteBoard msg sent back to clients", msg => {
+      this.updateSketchFieldWithSocketInfo(msg);
+    });
+  }
+
+    componentDidMount() {
+    // no given functions to listen keyEvents from canvas
+    // document.querySelector('.rdw-editor-toolbar').addEventListener('click', () => { 
+      
+    //   // Click events happen a couple milliseconds too early for fontsize/color/etc changes to register
+    //   setTimeout(() => {
+
+    //     socket.emit("word msg", {
+    //       room: this.props.roomName,
+    //       user: this.props.userName,
+    //       msg: convertToRaw(this.state.editorState.getCurrentContent())
+    //     });
+
+    //     }, 100)
+    //  });
+
+    // document.querySelector('.wordEditor').addEventListener('keydown', () => { 
+
+    //   socket.emit("word msg", {
+    //     room: this.props.roomName,
+    //     user: this.props.userName,
+    //     msg: convertToRaw(this.state.editorState.getCurrentContent())
+    //   });
+
+    //  });
+
+    // document.querySelector('.wordEditor').addEventListener('keyup', () => { 
+
+    //   socket.emit("word msg", {
+    //     room: this.props.roomName,
+    //     user: this.props.userName,
+    //     msg: convertToRaw(this.state.editorState.getCurrentContent())
+    //   });
+
+    // });
+  }
+
+  onSketchFieldChange = (data,__,___,____,_____) => {
+    console.log('​------------------------------------------------------------------------------------');
+    console.log('​WhiteBoardEditor -> onSketchFieldChange -> _,__,___,____,_____', data,__,___,____,_____);
+    console.log('​------------------------------------------------------------------------------------');
+
+    this.setState({sketchFieldValue: data, });
+  }
+
+  updateSketchFieldWithSocketInfo = (msg) => {
+    console.log('​----------------------------------------------------------------');
+    console.log('​WhiteBoardEditor -> updateSketchFieldWithSocketInfo -> msg', msg);
+    console.log('​----------------------------------------------------------------');
+    // this.setState({ editorState: EditorState.createWithContent(convertFromRaw(msg)),});
+  }
 
   render() {
+    console.log('​---------------------------------------------------------------------------------------');
+    console.log('​WhiteBoardEditor -> render -> this.state.sketchFieldValue', this.state.sketchFieldValue);
+    console.log('​---------------------------------------------------------------------------------------');
     return (
       <section className="whiteBoardContainer">
         <SketchField width="100vw"
           height="500px"
           tool={Tools.Pencil}
           lineColor="black"
-          lineWidth={6}/>
+          lineWidth={6}
+          value={this.sketchFieldValue}
+          forceValue={true}
+          onChange={this.onSketchFieldChange}/>
       </section>
     );
   }
@@ -32,3 +98,4 @@ const mapStateToProps = (state) => {
 };
 
 export default connect(mapStateToProps)(WhiteBoardEditor);
+
