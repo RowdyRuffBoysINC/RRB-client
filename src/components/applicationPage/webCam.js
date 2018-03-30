5a889dec7e51da00a0676b5f06287eb1a06465af
Abraham Cuenca <websiteprojectmanager@gmail.com>
2018-03-30 10:17:52 -0700

    Debugging video

--- src/components/applicationPage/webCam.js
+++ src/components/applicationPage/webCam.js
@@ -7,12 +7,20 @@ import './webCam.css'
 export class WebCam extends React.Component {
   
   componentWillMount () {
+    this.trace('I just executed component will mount');
     this._init();
   }
 
   componentDidUpdate() {
-    this._init();
+    this.trace('I just executed component did update')
+    this._update();
   }
+
+  trace(text){
+    text = text.trim();
+    const now = (window.performance.now() / 1000).toFixed(3);
+    console.log(now, text);
+  };
 
   _init() {
 
@@ -50,6 +58,7 @@ export class WebCam extends React.Component {
     const pc = new peerConnection({'iceServers': [{url: 'stun:stun1.l.google.com:19302'}]});
 
     pc.onaddstream = (obj) =>{
+      trace('added a stream')
       const vid = document.createElement('video');
       vid.setAttribute('class', 'video-small');
       vid.setAttribute('autoplay', 'autoplay');
@@ -59,6 +68,7 @@ export class WebCam extends React.Component {
     }
 
     navigator.getUserMedia({video: true}, (stream) => {
+      trace('get webcam')
       const videoBox = document.getElementById('video-box');
       const video = document.querySelector('video');
       video.src = window.URL.createObjectURL(stream);
@@ -68,12 +78,15 @@ export class WebCam extends React.Component {
     }, error);
 
     function error(err){
+      trace('some shit happened')
       console.error('Error', err);
     }
 
     const createOffer = (id) =>{
+      trace('creating an offer')
       pc.createOffer((offer) => {
         pc.setLocalDescription(new sessionDescription(offer), () => {
+          trace('making an offer locally')
           socket.emit('make-offer', {
             offer: offer,
             to: id,
@@ -84,6 +97,7 @@ export class WebCam extends React.Component {
     }
 
     socket.on('answer-made', (data) => {
+      trace('an answer was made')
       pc.setRemoteDescription(new sessionDescription(data.answer), () =>{
         document.getElementById(data.socket).setAttribute('class', 'active');
         if(!answersFrom[data.socket]){
@@ -94,11 +108,13 @@ export class WebCam extends React.Component {
     });
 
     socket.on('offer-made', (data) => {
+      trace('an offer was made')
       offer = data.offer;
 
       pc.setRemoteDescription(new sessionDescription(data.offer), () =>{
         pc.createAnswer((answer) => {
           pc.setLocalDescription(new sessionDescription(answer), () =>{
+            trace('making an answer')
             socket.emit('make-answer', {
               answer: answer,
               to:data.socket,
@@ -112,15 +128,16 @@ export class WebCam extends React.Component {
     socket.emit('add-users',{
       room: this.props.roomName, 
       user: this.props.username
-    })
+    }, trace('announcing a user'))
 
     socket.on('add-users', (data) =>{
+      trace('added a user from server')
       for(let i = 0; i < data.users.length; i++){
         const el = document.createElement('div'),
         id = data.users[i];
 
         el.setAttribute('id', id);
-        el.innerHTML = id; // This would come from logged in user name.
+        el.innerHTML = `The user with id ${id} is here (${data.user})`; // This would come from logged in user name.
         el.addEventListener('click', ()=>{
           createOffer(id);
         });
@@ -129,11 +146,120 @@ export class WebCam extends React.Component {
     });
 
     socket.on('remove-user', (id) =>{
+      trace('removing a user')
       let div = document.getElementById(id);
       if (div) document.getElementById('users').removeChild(div);
     });
 
 }
+
+_update() {
+
+  //#region Helpers
+      /**
+       * Helper functions to get this to work
+       **/
+  
+      const trace = text => {
+        text = text.trim();
+        const now = (window.performance.now() / 1000).toFixed(3);
+        console.log(now, text);
+      };
+  
+  //#endregion
+  
+      const socket = socketIOClient.connect('http://localhost:8080');
+      let answersFrom = {}, offer;
+  
+      const peerConnection = window.RTCPeerConnection ||
+      window.mozRTCPeerConnection ||
+      window.webKitRTCPeerConnection ||
+      window.msRTCPeerConnection;
+  
+      const sessionDescription = window.RTCSessionDescription ||
+      window.mozRTCSessionDescription ||
+      window.webkitRTCSessionDescription ||
+      window.msRTCSessionDescription
+  
+      navigator.getUserMedia = navigator.getUserMedia ||
+      navigator.webkitGetUSerMedia ||
+      navigator.mozGetUserMedia ||
+      navigator.msGetuserMedia;
+  
+      const pc = new peerConnection({'iceServers': [{url: 'stun:stun1.l.google.com:19302'}]});
+  
+      pc.onaddstream = (obj) =>{
+        trace('adding stream from update')
+        const vid = document.createElement('video');
+        vid.setAttribute('class', 'video-small');
+        vid.setAttribute('autoplay', 'autoplay');
+        vid.setAttribute('id', 'webCam-remoteVideo');
+        document.getElementById('video-box').appendChild(vid);
+        vid.src = window.URL.createObjectURL(obj.stream);
+      }
+  
+      navigator.getUserMedia({video: true}, (stream) => {
+        trace('getting camera from update')
+        const videoBox = document.getElementById('video-box');
+        const video = document.querySelector('video');
+        video.src = window.URL.createObjectURL(stream);
+        videoBox.appendChild(video);
+        video.load();
+        pc.addStream(stream);
+      }, error);
+  
+      function error(err){
+        trace('some shit went wrong in update')
+        console.error('Error', err);
+      }
+  
+      const createOffer = (id) =>{
+        pc.createOffer((offer) => {
+          trace('making a local offer from update')
+          pc.setLocalDescription(new sessionDescription(offer), () => {
+            socket.emit('make-offer', {
+              offer: offer,
+              to: id,
+              room: this.props.roomName
+            })
+          }, error);
+        }, error);
+      }
+  
+      socket.on('answer-made', (data) => {
+        pc.setRemoteDescription(new sessionDescription(data.answer), () =>{
+          document.getElementById(data.socket).setAttribute('class', 'active');
+          if(!answersFrom[data.socket]){
+            createOffer(data.socket);
+            answersFrom[data.socket] = true;
+          }
+        }, error);
+      });
+  
+      socket.on('offer-made', (data) => {
+        trace('an offer was made from update')
+        offer = data.offer;
+  
+        pc.setRemoteDescription(new sessionDescription(data.offer), () =>{
+          pc.createAnswer((answer) => {
+            pc.setLocalDescription(new sessionDescription(answer), () =>{
+              trace('an answer was made from update')
+              socket.emit('make-answer', {
+                answer: answer,
+                to:data.socket,
+                room: this.props.roomName
+              });
+            }, error)
+          }, error);
+        }, error);
+      });
+
+      socket.on('remove-user', (id) =>{
+        let div = document.getElementById(id);
+        if (div) document.getElementById('users').removeChild(div);
+      });
+  
+  }
 
   render() {
     return (
@@ -151,7 +277,7 @@ export class WebCam extends React.Component {
 }
 
 const mapStateToProps = (state) => ({
-  username: state.applicationReducer.username,
+  username: state.auth.currentUser.username,
   roomName: state.applicationReducer.roomName,
 });
 
