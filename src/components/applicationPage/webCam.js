99c35fb4a49518176ffcb82ceb4103652d1d1c25
Brent Guistwite <brentguistwite@gmail.com>
2018-04-02 14:21:47 -0400

    add audio

--- src/components/applicationPage/webCam.js
+++ src/components/applicationPage/webCam.js
@@ -15,69 +15,69 @@ export class WebCam extends React.Component {
     this.addedPersonId = '';
 
     this.peerConnection = window.RTCPeerConnection ||
-    window.mozRTCPeerConnection ||
-    window.webKitRTCPeerConnection ||
-    window.msRTCPeerConnection;
+      window.mozRTCPeerConnection ||
+      window.webKitRTCPeerConnection ||
+      window.msRTCPeerConnection;
 
     this.sessionDescription = window.RTCSessionDescription ||
-    window.mozRTCSessionDescription ||
-    window.webkitRTCSessionDescription ||
-    window.msRTCSessionDescription
+      window.mozRTCSessionDescription ||
+      window.webkitRTCSessionDescription ||
+      window.msRTCSessionDescription
 
     navigator.getUserMedia = navigator.getUserMedia ||
-    navigator.webkitGetUSerMedia ||
-    navigator.mozGetUserMedia ||
-    navigator.msGetuserMedia;
+      navigator.webkitGetUSerMedia ||
+      navigator.mozGetUserMedia ||
+      navigator.msGetuserMedia;
 
     this.navigator = navigator;
 
-    this.pc = new this.peerConnection({'iceServers': [{url: 'stun:stun1.l.google.com:19302'}]});
+    this.pc = new this.peerConnection({ 'iceServers': [{ url: 'stun:stun1.l.google.com:19302' }] });
 
   }
 
-  componentWillMount () {
-    
+  componentWillMount() {
+
     this._init();
 
   }
 
-  trace(text){
+  trace(text) {
     text = text.trim();
     const now = (window.performance.now() / 1000).toFixed(3);
     console.log(now, text);
   };
 
-createVideo(id) {
+  createVideo(id) {
 
-  let answersFrom = {}, offer;
+    let answersFrom = {}, offer;
 
-  function error(err){
-    trace('some shit happened');
-    console.error('Error', err);
+    function error(err) {
+      trace('some shit happened');
+      console.error('Error', err);
+    }
+
+    function trace(text) {
+      text = text.trim();
+      const now = (window.performance.now() / 1000).toFixed(3);
+    };
+
+
+    this.pc.createOffer((offer) => {
+      this.pc.setLocalDescription(new this.sessionDescription(offer), () => {
+        trace('making an offer locally')
+        socket.emit('make-offer', {
+          offer: offer,
+          to: id,
+          room: this.props.roomName
+        })
+      }, error);
+    }, error);
+
+
   }
 
-  function trace(text){
-    text = text.trim();
-    const now = (window.performance.now() / 1000).toFixed(3);
-  };
 
-
-  this.pc.createOffer((offer) => {
-    this.pc.setLocalDescription(new this.sessionDescription(offer), () => {
-      trace('making an offer locally')
-      socket.emit('make-offer', {
-        offer: offer,
-        to: id,
-        room: this.props.roomName
-      })
-    }, error);
-  }, error);
-
-
-}
-
-
-_init() {
+  _init() {
 
     //#region Helpers
     /**
@@ -89,108 +89,108 @@ _init() {
       const now = (window.performance.now() / 1000).toFixed(3);
     };
 
-//#endregion
-  let answersFrom = {}, offer;
+    //#endregion
+    let answersFrom = {}, offer;
 
-  this.pc.onaddstream = (obj) =>{
-    trace('added a stream'); 
-    const vid = document.createElement('video');
-    vid.setAttribute('class', 'video-small');
-    vid.setAttribute('autoplay', 'autoplay');
-    vid.setAttribute('id', `video-${this.addedPersonId}`);
-    if (this.addedPersonId) 
-      vid.setAttribute("key", `${this.addedPersonId}`);
-    
-    document.getElementById('video-box').appendChild(vid);
-    vid.src = window.URL.createObjectURL(obj.stream);
-  }
+    this.pc.onaddstream = (obj) => {
+      trace('added a stream');
+      const vid = document.createElement('video');
+      vid.setAttribute('class', 'video-small');
+      vid.setAttribute('autoplay', 'autoplay');
+      vid.setAttribute('id', `video-${this.addedPersonId}`);
+      if (this.addedPersonId)
+        vid.setAttribute("key", `${this.addedPersonId}`);
 
-  this.navigator.getUserMedia({video: true}, (stream) => {
-    trace('get webcam FROM INIT')
-    const videoBox = document.getElementById('video-box');
-    const video = document.querySelector('video');
-    video.src = window.URL.createObjectURL(stream);
-    videoBox.appendChild(video);
-    video.load();
-    this.pc.addStream(stream);
-  }, error);
+      document.getElementById('video-box').appendChild(vid);
+      vid.src = window.URL.createObjectURL(obj.stream);
+    }
 
-  function error(err){
-    trace('some shit happened');
-    console.error('Error', err);
-  }
+    this.navigator.getUserMedia({ video: true, audio: true, }, (stream) => {
+      trace('get webcam FROM INIT')
+      const videoBox = document.getElementById('video-box');
+      const video = document.querySelector('video');
+      video.src = window.URL.createObjectURL(stream);
+      videoBox.appendChild(video);
+      video.load();
+      this.pc.addStream(stream);
+    }, error);
 
-  const createOffer = (id) =>{
-    trace('creating an offer')
-    this.pc.createOffer((offer) => {
-      this.pc.setLocalDescription(new this.sessionDescription(offer), () => {
-        trace('making an offer locally')
-        socket.emit('make-offer', {
-          offer: offer,
-          to: id,
-          room: this.props.roomName
-        })
+    function error(err) {
+      trace('some shit happened');
+      console.error('Error', err);
+    }
+
+    const createOffer = (id) => {
+      trace('creating an offer')
+      this.pc.createOffer((offer) => {
+        this.pc.setLocalDescription(new this.sessionDescription(offer), () => {
+          trace('making an offer locally')
+          socket.emit('make-offer', {
+            offer: offer,
+            to: id,
+            room: this.props.roomName
+          })
+        }, error);
       }, error);
-    }, error);
-  }
+    }
 
-  socket.on('answer-made', (data) => {
+    socket.on('answer-made', (data) => {
 
-    trace('an answer was made FROM INIT');
-    
-    this.pc.setRemoteDescription(new this.sessionDescription(data.answer), () => {
-      if(!answersFrom[data.socket]){
-        createOffer(data.socket);
-        answersFrom[data.socket] = true;
-      }
-    }, error);
-  });
+      trace('an answer was made FROM INIT');
 
-  socket.on('offer-made', (data) => {
-    trace('an offer was made FROM INIT')
-    offer = data.offer;
-
-    this.pc.setRemoteDescription(new this.sessionDescription(data.offer), () =>{
-      this.pc.createAnswer((answer) => {
-        this.pc.setLocalDescription(new this.sessionDescription(answer), () =>{
-          trace('making an answer')
-          socket.emit("make-answer", {
-            answer: answer,
-            to: data.socket,
-            room: this.props.roomName,
-            user: this.props.username
-          });
-        }, error)
+      this.pc.setRemoteDescription(new this.sessionDescription(data.answer), () => {
+        if (!answersFrom[data.socket]) {
+          createOffer(data.socket);
+          answersFrom[data.socket] = true;
+        }
       }, error);
-    }, error);
-  });
+    });
 
-  socket.emit('add-users',{
-    room: this.props.roomName, 
-    user: this.props.username
-  }, trace('announcing a user FROM INIT'))
+    socket.on('offer-made', (data) => {
+      trace('an offer was made FROM INIT')
+      offer = data.offer;
 
-  socket.on('add-users', (data) =>{
-    this.props.dispatch(ApplicationActions.setUserList(data.users));
-  });
+      this.pc.setRemoteDescription(new this.sessionDescription(data.offer), () => {
+        this.pc.createAnswer((answer) => {
+          this.pc.setLocalDescription(new this.sessionDescription(answer), () => {
+            trace('making an answer')
+            socket.emit("make-answer", {
+              answer: answer,
+              to: data.socket,
+              room: this.props.roomName,
+              user: this.props.username
+            });
+          }, error)
+        }, error);
+      }, error);
+    });
 
-  socket.on("remove-user", id => {
-    this.props.dispatch(ApplicationActions.deleteUserFromList(id));
-  });
+    socket.emit('add-users', {
+      room: this.props.roomName,
+      user: this.props.username
+    }, trace('announcing a user FROM INIT'))
+
+    socket.on('add-users', (data) => {
+      this.props.dispatch(ApplicationActions.setUserList(data.users));
+    });
+
+    socket.on("remove-user", id => {
+      this.props.dispatch(ApplicationActions.deleteUserFromList(id));
+    });
 
 
   }
 
-render() {
+  render() {
     return (
       <div className="webCam-container">
-      <div className="video-box" id="video-box">
-        <video className="video-large" id="webCam-localVideo" autoPlay></video>
-      </div>
-      <div className="users-container" id="users-container">
-        <h4> Room: {this.props.roomName} </h4>
-        <UsersList createOffer={(id) => this.createVideo(id)}/>
-      </div>
+        <div className="video-box" id="video-box">
+          <video className="video-large" id="webCam-localVideo" autoPlay></video>
+        </div>
+        <div className="users-container" id="users-container">
+          <h4> Room: {this.props.roomName} </h4>
+          <UsersList createOffer={(id) => this.createVideo(id)} />
+        </div>
       </div>
     );
   }
