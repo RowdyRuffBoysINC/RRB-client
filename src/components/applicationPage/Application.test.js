52d286cf1f7f87f7d71763b35ca5eee4c62552d0
Abraham Cuenca <websiteprojectmanager@gmail.com>
2018-04-02 10:26:01 -0700

    Add Application test

--- src/components/applicationPage/Application.test.js
+++ src/components/applicationPage/Application.test.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import thunk from 'redux-thunk';
 import { shallow, } from 'enzyme';
-import configureMockStore from 'redux-mock-store';
 import '../../setupTests.js';
-import Application from './Application.js';
+import {Dashboard,} from './Application.js';
 
-const initialState = {};
-
-const middlewares = [ thunk , ];
-const mockStore = configureMockStore(middlewares);
-let store;
-let wrapper;
-
-beforeEach(() =>{
-  store = mockStore(initialState);
-});
-
-describe ('<Application />', () =>{
+describe ('<Dashboard />', () =>{
   it('passes a smoke test', () =>{
     // Add Application test here
+    shallow(<Dashboard match={{url: '/dsahboard',}} dispatch={jest.fn}/>);
   });
 });
 
