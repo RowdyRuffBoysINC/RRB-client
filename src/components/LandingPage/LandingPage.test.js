e223e6417624e531305ad1ca546f56843c9c61ae
Abraham Cuenca <websiteprojectmanager@gmail.com>
2018-04-03 22:40:27 -0700

    Update LandingPage test

--- src/components/LandingPage/LandingPage.test.js
+++ src/components/LandingPage/LandingPage.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {shallow,} from 'enzyme';
-import '../setupTests.js';
+import '../../setupTests.js';
 import {LandingPage,} from './LandingPage';
 
 describe('<LandingPage />', () => {
