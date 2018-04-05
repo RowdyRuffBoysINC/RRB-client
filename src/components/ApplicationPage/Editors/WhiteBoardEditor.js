2a3569db5a088855c020da0e50d8466efd7c1f81
Vernon Mensah <vernonmensah@gmail.com>
2018-04-05 14:36:24 -0400

    no need for state or interval

--- src/components/ApplicationPage/Editors/WhiteBoardEditor.js
+++ src/components/ApplicationPage/Editors/WhiteBoardEditor.js
@@ -2,15 +2,13 @@ import React from 'react';
 import { connect, } from 'react-redux';
 import { socket, } from '../Room';
 import { SketchField, Tools, } from 'react-sketch';
-import { setWhiteBoardEditorValue } from "../../../actions/Editor";
+import { setWhiteBoardEditorValue, } from '../../../actions/Editor';
 import './WhiteBoardEditor.css';
 
 export class WhiteBoardEditor extends React.Component {
   constructor() {
     super();
-    this.state = { sketchFieldValue: null, };
     this.sketch = null;
-    this.interval = null;
 
     socket.on('whiteBoard msg sent back to clients', (msg) => {
       this.updateSketchFieldWithSocketInfo(msg);
@@ -20,24 +18,24 @@ export class WhiteBoardEditor extends React.Component {
   componentDidMount() {
     // No given functions to listen keyEvents from canvas
 
-    document.querySelector(".upper-canvas").addEventListener("click", () => {
-      
+    document.querySelector('.upper-canvas').addEventListener('click', () => {
+
       if (this.sketch) {
-        this.sendMessage("whiteBoard msg", this.sketch.toJSON(this.props.whiteBoardEditorValue));
+        this.sendMessage('whiteBoard msg', this.sketch.toJSON(this.props.whiteBoardEditorValue));
       }
     });
 
     document.querySelector('.upper-canvas').addEventListener('mousedown', () => {
 
       if (this.sketch) {
-        this.sendMessage("whiteBoard msg", this.sketch.toJSON(this.props.whiteBoardEditorValue));
+        this.sendMessage('whiteBoard msg', this.sketch.toJSON(this.props.whiteBoardEditorValue));
       }
     });
 
     document.querySelector('.upper-canvas').addEventListener('mouseup', () => {
 
       if (this.sketch) {
-        this.sendMessage("whiteBoard msg", this.sketch.toJSON(this.props.whiteBoardEditorValue));
+        this.sendMessage('whiteBoard msg', this.sketch.toJSON(this.props.whiteBoardEditorValue));
       }
 
     });
@@ -45,7 +43,7 @@ export class WhiteBoardEditor extends React.Component {
     document.querySelector('.upper-canvas').addEventListener('mouseleave', () => {
 
       if (this.sketch) {
-        this.sendMessage("whiteBoard msg", this.sketch.toJSON(this.props.whiteBoardEditorValue));
+        this.sendMessage('whiteBoard msg', this.sketch.toJSON(this.props.whiteBoardEditorValue));
       }
     });
   }
@@ -78,7 +76,7 @@ export class WhiteBoardEditor extends React.Component {
           tool={Tools.Pencil}
           lineColor="black"
           lineWidth={6}
-          ref={(instance) => this.sketch = instance}
+          ref={instance => this.sketch = instance}
           forceValue={true}
           onChange={data => this.onSketchFieldChange(data)} />
       </section>
@@ -91,7 +89,7 @@ const mapStateToProps = (state) => {
     username: state.auth.currentUser.username,
     name: `${state.auth.currentUser.firstName} ${state.auth.currentUser.lastName}`,
     roomName: state.applicationReducer.roomName,
-    whiteBoardEditorValue: state.editorReducer.whiteBoardEditorValue
+    whiteBoardEditorValue: state.editorReducer.whiteBoardEditorValue,
   };
 };
 
