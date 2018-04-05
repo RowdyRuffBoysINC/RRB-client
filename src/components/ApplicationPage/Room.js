d749e195abdf8aba3bb8ecc86f10a74a0a35425d
David Park <dysp92@gmail.com>
2018-04-05 16:26:30 -0700

    added fetch docs action on componentWillMount

--- src/components/ApplicationPage/Room.js
+++ src/components/ApplicationPage/Room.js
@@ -5,13 +5,17 @@ import io from 'socket.io-client';
 import { withRouter, } from 'react-router';
 import EditorView from './EditorView';
 import { setCreateInput, } from '../../actions/Application';
-import { saveDocsToDb, } from '../../actions/Editor';
+import { fetchDocsFromDb, saveDocsToDb, } from '../../actions/Editor';
 import { API_BASE_URL, } from '../../config';
 import WebCam from './WebCam';
 export const socket = io(API_BASE_URL);
 
 
 export class Room extends React.Component {
+  componentWillMount() {
+    this.props.dispatch(fetchDocsFromDb(this.props.match.params.roomName));
+  }
+
   componentDidMount() {
     this.props.dispatch(setCreateInput(this.props.match.params.roomName));
     socket.emit('join room', { room: this.props.match.params.roomName, user: this.props.username, });
