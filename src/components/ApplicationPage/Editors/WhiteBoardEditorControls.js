e79d222c28b171b936ccb6738c991c087177a81c
Vernon Mensah <vernonmensah@gmail.com>
2018-04-05 16:25:44 -0400

    component to function

--- src/components/ApplicationPage/Editors/WhiteBoardEditorControls.js
+++ src/components/ApplicationPage/Editors/WhiteBoardEditorControls.js
@@ -3,43 +3,41 @@ import { connect, } from 'react-redux';
 import { setWhiteBoardEditorColor, setWhiteBoardEditorBrushSize, } from '../../../actions/Editor';
 import './WhiteBoardEditorControls.css';
 
-export class WhiteBoardEditorControls extends React.Component {
-
-  changeColor(color) {
+export function WhiteBoardEditorControls(props) {
+  function changeColor(color) {
     console.log(color);
-    this.props.dispatch(setWhiteBoardEditorColor(color));
+    props.dispatch(setWhiteBoardEditorColor(color));
   }
 
-  changeFontSize() {
-    if (this.props.whiteBoardEditorBrushSize === 12) {
-      this.props.dispatch(setWhiteBoardEditorBrushSize(2));
+  function changeFontSize() {
+    if (props.whiteBoardEditorBrushSize === 12) {
+      props.dispatch(setWhiteBoardEditorBrushSize(2));
     }
 
     else {
-      this.props.dispatch(setWhiteBoardEditorBrushSize(this.props.whiteBoardEditorBrushSize + 2));
+      props.dispatch(setWhiteBoardEditorBrushSize(props.whiteBoardEditorBrushSize + 2));
     }
   }
 
-  changeBrushToEraser() {
-    this.props.dispatch(setWhiteBoardEditorColor('white'));
+  function changeBrushToEraser() {
+    props.dispatch(setWhiteBoardEditorColor('white'));
   }
 
-  render() {
-    const { whiteBoardEditorBrushSize, } = this.props;
-    
-    return (
-      <section className="whiteBoardControls-container">
-        <div onClick={() => this.changeBrushToEraser()} className="tool eraser"></div>
-        <div onClick={() => this.changeColor('green')} className="tool green-brush"></div>
-        <div onClick={() => this.changeColor('red')} className="tool red-brush"></div>
-        <div onClick={() => this.changeColor('blue')} className="tool blue-brush"></div>
-        <div onClick={() => this.changeColor('yellow')} className="tool yellow-brush"></div>
-        <div onClick={() => this.changeColor('black')} className="tool black-brush"></div>
-        <div onClick={() => this.changeFontSize()} className="tool change-size-button"> {whiteBoardEditorBrushSize} </div>
-      </section>
-    );
-  }
+  const { whiteBoardEditorBrushSize, } = props;
+
+  return (
+    <section className="whiteBoardControls-container">
+      <div onClick={() => changeBrushToEraser()} className="tool eraser"></div>
+      <div onClick={() => changeColor('green')} className="tool green-brush"></div>
+      <div onClick={() => changeColor('red')} className="tool red-brush"></div>
+      <div onClick={() => changeColor('blue')} className="tool blue-brush"></div>
+      <div onClick={() => changeColor('yellow')} className="tool yellow-brush"></div>
+      <div onClick={() => changeColor('black')} className="tool black-brush"></div>
+      <div onClick={() => changeFontSize()} className="tool change-size-button"> {whiteBoardEditorBrushSize} </div>
+    </section>
+  );
 }
+
 
 const mapStateToProps = (state) => {
   return {
