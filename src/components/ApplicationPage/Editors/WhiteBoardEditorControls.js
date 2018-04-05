ede98f170d694434e30bb94da0695df888dbac90
Vernon Mensah <vernonmensah@gmail.com>
2018-04-05 15:02:41 -0400

    added whiteBoardEditorControls to paint on white board

---
+++ src/components/ApplicationPage/Editors/WhiteBoardEditorControls.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { connect, } from 'react-redux';
+import { socket, } from '../Room';
+import { SketchField, Tools, } from 'react-sketch';
+import { setWhiteBoardEditorColor, setWhiteBoardEditorBrushSize, } from '../../../actions/Editor';
+import './WhiteBoardEditorControls.css';
+
+export class WhiteBoardEditorControls extends React.Component {
+  render() {
+    return (
+      <section className="whiteboardControls-container">
+        <div className="tool eraser"></div>
+        <div className="tool green-brush"></div>
+        <div className="tool red-brush"></div>
+        <div className="tool blue-brush"></div>
+        <div className="tool yellow-brush"></div>
+        <div className="tool green-brush" />
+        <div className="tool change-size-button"> {this.props.setWhiteBoardEditorColor} </div>
+      </section>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    whiteBoardEditorBrushSize: state.editorReducer.whiteBoardEditorBrushSize,
+    whiteBoardEditorColor: state.editorReducer.whiteBoardEditorColor,
+  };
+};
+
+export default connect(mapStateToProps)(WhiteBoardEditorControls);
