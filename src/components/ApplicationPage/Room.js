403bd295ebf3b5cb0828f88c620edc214a27d8f1
David Park <dysp92@gmail.com>
2018-04-05 16:00:31 -0700

    saving doc every 5min

--- src/components/ApplicationPage/Room.js
+++ src/components/ApplicationPage/Room.js
@@ -5,6 +5,7 @@ import io from 'socket.io-client';
 import { withRouter, } from 'react-router';
 import EditorView from './EditorView';
 import { setCreateInput, } from '../../actions/Application';
+import { saveDocsToDb, } from '../../actions/Editor';
 import { API_BASE_URL, } from '../../config';
 import WebCam from './WebCam';
 export const socket = io(API_BASE_URL);
@@ -14,10 +15,17 @@ export class Room extends React.Component {
   componentDidMount() {
     this.props.dispatch(setCreateInput(this.props.match.params.roomName));
     socket.emit('join room', { room: this.props.match.params.roomName, user: this.props.username, });
+    const doc = {
+      code: this.props.codeEditorText,
+      word: this.props.wordEditorText,
+      whiteBoard: this.props.whiteBoardEditorText,
+    };
+    this.interval = setInterval(this.props.dispatch(saveDocsToDb(doc)), 30000);
   }
 
   componentWillUnmount() {
     socket.emit('leave room', { room: this.props.match.params.roomName, user: this.props.username, });
+    clearInterval(this.interval);
   }
 
   render() {
@@ -33,6 +41,9 @@ export class Room extends React.Component {
 const mapStateToProps = state => ({
   username: state.auth.currentUser.username,
   roomName: state.applicationReducer.roomName,
+  codeEditorText: state.editorReducer.codeEditorText,
+  wordEditorText: state.editorReducer.wordEditorText,
+  whiteBoardEditorText: state.editorReducer.whiteBoardEditorText,
 });
 
 export default withRouter((connect)(mapStateToProps)(Room));
