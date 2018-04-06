7ec1e696c768131946382dd6b9ff2a0951d4b67d
David Park <dysp92@gmail.com>
2018-04-06 08:06:14 -0700

    post endpoint fix

--- src/components/ApplicationPage/Room.js
+++ src/components/ApplicationPage/Room.js
@@ -5,7 +5,7 @@ import io from 'socket.io-client';
 import { withRouter, } from 'react-router';
 import EditorView from './EditorView';
 import { setCreateInput, } from '../../actions/Application';
-import { fetchDocsFromDb, saveDocsToDb, } from '../../actions/Editor';
+import { fetchDocsFromDb, updateDocsDb, createDocsDb, } from '../../actions/Editor';
 import { API_BASE_URL, } from '../../config';
 import WebCam from './WebCam';
 export const socket = io(API_BASE_URL);
@@ -13,19 +13,24 @@ export const socket = io(API_BASE_URL);
 
 export class Room extends React.Component {
   componentWillMount() {
-    console.log(this.props.match.params.roomName);
-    if (this.props.dispatch(fetchDocsFromDb(this.props.match.params.roomName))) {
-      console.log(true);
-    } else {
-      console.log(false);
-    }
+    this.props.dispatch(fetchDocsFromDb(this.props.match.params.roomName))
+      .then((value) => {
+        if (value === false) {
+          this.props.dispatch(createDocsDb({
+            roomName: this.props.roomName,
+            codeEditorText: this.props.codeEditorText,
+            wordEditorText: this.props.wordEditorText,
+            whiteBoardEditorText: this.props.whiteBoardEditorText,    
+          }));
+        }
+      });
   }
 
   componentDidMount() {
     this.props.dispatch(setCreateInput(this.props.match.params.roomName));
     socket.emit('join room', { room: this.props.match.params.roomName, user: this.props.username, });
     this.interval = setInterval(() => {
-      this.props.dispatch(saveDocsToDb({
+      this.props.dispatch(updateDocsDb({
         roomName: this.props.roomName,
         codeEditorText: this.props.codeEditorText,
         wordEditorText: this.props.wordEditorText,
