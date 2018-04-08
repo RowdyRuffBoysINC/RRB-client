d3bfc0b45f2514c1c12ce88574df6f447ab563aa
Brent Guistwite <brentguistwite@gmail.com>
2018-04-08 14:18:38 -0400

    update action to now take in message

--- src/actions/Chat.js
+++ src/actions/Chat.js
@@ -1,2 +1,5 @@
 export const UPDATE_CHAT_LOG = 'UPDATE_CHAT_LOG';
-export const updateChatLog = () => ({ type: UPDATE_CHAT_LOG, });
+export const updateChatLog = message => ({
+  type: UPDATE_CHAT_LOG,
+  message,
+});
