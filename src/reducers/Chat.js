19496313c97c52ffe080fbaea5cb39ee2c487cc6
Brent Guistwite <brentguistwite@gmail.com>
2018-04-08 13:32:11 -0400

    set up basic chatReducer

--- src/reducers/Chat.js
+++ src/reducers/Chat.js
@@ -2,3 +2,10 @@ const initialState = {
   messageLogs: [],
   messageDraft: '',
 };
+
+const chatReducer = function (state = initialState, action) {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
