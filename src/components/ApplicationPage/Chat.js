b0549372b9e34d0b959f2f4b3e808c38ef4be39c
Brent Guistwite <brentguistwite@gmail.com>
2018-04-08 14:16:12 -0400

    connect chat component to test with reducer using console logs

--- src/components/ApplicationPage/Chat.js
+++ src/components/ApplicationPage/Chat.js
@@ -1,20 +1,51 @@
 import React from 'react';
+import { connect, } from 'react-redux';
 
+import { socket, } from './Room';
 import { updateChatLog, } from './../../actions/Chat';
 import './Chat.css';
 
 
-export default function Chat(props) {
-  return (
-    <section className="chat-wrapper">
-      <div className="chat-display">
-        <ul className="chat-messages">
-          <li>
-            Username: message from user
-          </li>
-        </ul>
-      </div>
-      <input className="chat-user-input" placeholder="Type here..." />
-    </section>
-  );
+export class Chat extends React.Component {
+  handleSubmit(event) {
+    event.preventDefault();
+    const answer = this.input.value;
+    console.log(answer);
+    this.props.dispatch(updateChatLog());
+  }
+
+  render() {
+    return (
+      <section className="chat-wrapper">
+        <div className="chat-display">
+          <ul className="chat-messages">
+            <li>
+              Username: message from user
+            </li>
+          </ul>
+        </div>
+        <form
+          onSubmit={(event) => {
+            this.handleSubmit(event);
+          }}>
+          <input
+            className="chat-user-input"
+            placeholder="Type here..."
+            ref={(input) => {
+              this.input = input;
+            }}
+          />
+        </form>
+      </section>
+    );
+  }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    username: state.auth.currentUser.username,
+    name: `${state.auth.currentUser.firstName} ${state.auth.currentUser.lastName}`,
+  };
+};
+
+export default connect(mapStateToProps)(Chat);
--- src/reducers/Chat.js
+++ src/reducers/Chat.js
@@ -8,7 +8,7 @@ const chatReducer = function (state = initialState, action) {
   switch (action.type) {
   case UPDATE_CHAT_LOG:
     console.log('Chat wired up!');
-    break;
+    return state;
   default:
     return state;
   }
