8095a2bc23544e52f99df25b719e0f4cf9541cd5
Brent Guistwite <brentguistwite@gmail.com>
2018-04-10 11:15:10 -0400

    write explanation for eslint disable

--- src/components/ApplicationPage/WebCam/helpers.js
+++ src/components/ApplicationPage/WebCam/helpers.js
@@ -1,26 +1,32 @@
+/*
+eslint-disable
+*/
+
 /**
  *
  * @param {*} text a message to return as text
  * @param {*} level valid options are info, log, warn, error
  */
-/* eslint-disable */
-export const trace = (text, level='info') => {
-  if(typeof text === 'string') text = text.trim();
+
+// Trace function used throughout code to provide helpful logs.
+// In this instance its acceptable to have console logs in production
+export const trace = (text, level = 'info') => {
+  if (typeof text === 'string') text = text.trim();
   const now = (window.performance.now() / 1000).toFixed(3);
 
-  switch(level.toLowerCase().trim()) {
-  case 'info':
-    console.info(now, text);
-    break;
-  case 'log':
-    console.log(now, text);
-    break;
-  case 'warn':
-    console.warn(now, text);
-    break;
-  case 'error':
-    console.error(now, text);
-    break;
+  switch (level.toLowerCase().trim()) {
+    case 'info':
+      console.info(now, text);
+      break;
+    case 'log':
+      console.log(now, text);
+      break;
+    case 'warn':
+      console.warn(now, text);
+      break;
+    case 'error':
+      console.error(now, text);
+      break;
   }
 };
 
@@ -30,4 +36,4 @@ export const trace = (text, level='info') => {
  */
 export const error = (message) => {
   trace(message, 'error');
-}
+};
