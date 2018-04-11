0135d9e5ad45fc5e6fafdc12196bf0a86adea5af
Jeff Connelly <jeff.connelly6@gmail.com>
2018-04-11 10:57:35 -0600

    removed view text

--- src/components/ApplicationPage/Chat.js
+++ src/components/ApplicationPage/Chat.js
@@ -68,8 +68,6 @@ export class Chat extends React.Component {
           <input
             className="chat-user-input"
             placeholder="Type here..."
-            onFocus={e => e.target.placeholder = ''}
-            onBlur={e => e.target.placeholder = 'Type here...'}
             ref={(input) => {
               this.input = input;
             }}
