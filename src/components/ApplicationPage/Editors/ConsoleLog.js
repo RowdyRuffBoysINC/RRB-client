52fda77d33657eaebad1ad245d2bc997f4ead8f4
Vernon Mensah <vernonmensah@gmail.com>
2018-04-12 13:57:19 -0400

    consoleLog component

---
+++ src/components/ApplicationPage/Editors/ConsoleLog.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { connect, } from 'react-redux';
+
+export function ConsoleLog(props) {
+  const { userList, } = props;
+
+  const consoleLog = userList.map((user) => {
+    if (user.user === props.username) {
+      return <li className="user" key={user.id}>{user.user}</li>;
+    }
+    else {
+      return <li onClick={() => props.createVideoFunc(user.id)} key={user.id}>Share your camera with {user.user}</li>;
+    }
+  });
+  return (<ul>{consoleLog}</ul>);
+}
+
+const mapStateToProps = (state) => {
+  return {
+    username: state.auth.currentUser.username,
+    name: `${state.auth.currentUser.firstName} ${state.auth.currentUser.lastName}`,
+    userList: state.applicationReducer.listOfUsers,
+    createVideoFunc: state.applicationReducer.createVideoFunc,
+  };
+};
+
+export default connect(mapStateToProps)(ConsoleLog);
